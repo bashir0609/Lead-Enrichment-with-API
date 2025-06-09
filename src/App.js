8e28ea7a89e6dd88eb8ea986ae326a280b41d9c6
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-06-10 00:16:34 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import LeadEnrichmentPlatform from './components/LeadEnrichmentPlatform';
 
+function App() {
+  return (
+    <div className="App">
+      <LeadEnrichmentPlatform />
+    </div>
+  );
+}
+
+export default App;
